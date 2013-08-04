a7aad8a8f99d129cc463a5d183b14413e80ac7d4
David Graham <david@oninmedia.com>
2013-08-04 19:12:00 +0100

    .

---
+++ Mystery Shopper/OIMViewController.h
@@ -0,0 +1,13 @@
+//
+//  OIMViewController.h
+//  Mystery Shopper
+//
+//  Created by David Graham on 04/08/2013.
+//  Copyright (c) 2013 ON.IN.MEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OIMViewController : UIViewController
+
+@end
---
+++ Mystery Shopper/OIMViewController.m
@@ -0,0 +1,29 @@
+//
+//  OIMViewController.m
+//  Mystery Shopper
+//
+//  Created by David Graham on 04/08/2013.
+//  Copyright (c) 2013 ON.IN.MEDIA. All rights reserved.
+//
+
+#import "OIMViewController.h"
+
+@interface OIMViewController ()
+
+@end
+
+@implementation OIMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
